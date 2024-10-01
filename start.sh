d383848fecfc83da3d08b221ad34a2a3ffb46bba
chai35217793 <149489960+chai35217793@users.noreply.github.com>
2024-10-01 21:15:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ git clone https://github.com/chai35217793/UPLOADER-BOT-V9-is /UPLOADER-BOT-V9-is
 cd /UPLOADER-BOT-V9-is
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by @LISA_FAN_KL"
-gunciorn app:app & python3 bot.py
+python3 bot.py
