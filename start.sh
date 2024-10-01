338388e71513d1b1e10f7f2e0c530b2da9ab8545
chai35217793 <149489960+chai35217793@users.noreply.github.com>
2024-10-01 21:40:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/chai35217793/UPLOADER-BOT-V9-is /UPLOADER-BOT-V9-is
-cd /UPLOADER-BOT-V9-is
+git clone https://github.com/LISA-KOREA/UPLOADER-BOT-V3 /UPLOADER-BOT-V3
+cd /UPLOADER-BOT-V3
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by @LISA_FAN_KL"
 gunicorn app:app & python3 bot.py
